97113a2574517e0fd6016c14ed2d4d304b324d27
senshirou <senshirou@hotmail.com>
2019-09-30 23:36:58 +0900

    Object synchronization

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		time += Time.deltaTime;
+
+		if(time >= 1f)
+		{
+			Vector3 color = new Vector3(Random.value,Random.value,Random.value);
+			this.GetComponent<Renderer>().material.color = new Color(color.x,color.y,color.z);
+			time = 0;
+			
+		}
+
+		Debug.Log(time);
+		
+	}
+}
